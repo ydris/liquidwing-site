b741e79f84617d3b2d4e85ff0e39d7e67154c4df
ydris <davidashton68@gmail.com>
2025-06-27 22:47:46 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="LiquidWing — Coming Real Soon" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing the team</code>
         </p>
       </main>
 
