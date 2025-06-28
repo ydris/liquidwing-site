b2bc68cb0290be8d2f2b106bb2c3a84f7f29f657
ydris <davidashton68@gmail.com>
2025-06-27 22:38:23 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>LiquidWing — Coming Soon</title>
+        <title>LiquidWing — Coming Real Soon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
